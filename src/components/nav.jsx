e305eaa13683ee3e79cd07fb2255f4c3cad21ffb
Juan Lopez <aragonlopezjuan87@gmail.com>
2021-10-11 22:31:54 +0200

    feat: create link to nav

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Box, Text } from '@chakra-ui/react'
 import { BsHouse, BsHeart, BsPlusCircle, BsEnvelope, BsPerson } from "react-icons/bs";
+import {
+	Link
+} from "react-router-dom";
 
 export const Nav = () => {
 	return (
@@ -15,26 +18,36 @@ export const Nav = () => {
 			borderColor='gray.200'
 			p='1rem'
 		>
-			<Box display='flex' flexDirection='column' justifyContent='center' alignItems='center' >
-				<BsHouse />
-				<Text fontSize="xs" paddingTop='.3rem'>Inicio</Text>
-			</Box>
-			<Box display='flex' flexDirection='column' justifyContent='center' alignItems='center'  >
-				<BsHeart />
-				<Text fontSize="xs" paddingTop='.3rem'>Favoritos</Text>
-			</Box>
-			<Box display='flex' flexDirection='column' justifyContent='center' alignItems='center' >
-				<BsPlusCircle />
-				<Text fontSize="xs" paddingTop='.3rem'>Súbelo</Text>
-			</Box>
-			<Box display='flex' flexDirection='column' justifyContent='center' alignItems='center' >
-				<BsEnvelope />
-				<Text fontSize="xs" paddingTop='.3rem'>Buzon</Text>
-			</Box>
-			<Box display='flex' flexDirection='column' justifyContent='center' alignItems='center' >
-				<BsPerson />
-				<Text fontSize="xs" paddingTop='.3rem'>Tú</Text>
-			</Box>
+			<Link to="/">
+				<Box display='flex' flexDirection='column' justifyContent='center' alignItems='center' >
+					<BsHouse />
+					<Text fontSize="xs" paddingTop='.3rem'>Inicio</Text>
+				</Box>
+			</Link>
+			<Link to="/favorite">
+				<Box display='flex' flexDirection='column' justifyContent='center' alignItems='center'  >
+					<BsHeart />
+					<Text fontSize="xs" paddingTop='.3rem'>Favoritos</Text>
+				</Box>
+			</Link>
+			<Link to="/upload">
+				<Box display='flex' flexDirection='column' justifyContent='center' alignItems='center' >
+					<BsPlusCircle />
+					<Text fontSize="xs" paddingTop='.3rem'>Súbelo</Text>
+				</Box>
+			</Link>
+			<Link to="/mailbox">
+				<Box display='flex' flexDirection='column' justifyContent='center' alignItems='center' >
+					<BsEnvelope />
+					<Text fontSize="xs" paddingTop='.3rem'>Buzon</Text>
+				</Box>
+			</Link>
+			<Link to="/you">
+				<Box display='flex' flexDirection='column' justifyContent='center' alignItems='center' >
+					<BsPerson />
+					<Text fontSize="xs" paddingTop='.3rem'>Tú</Text>
+				</Box>
+			</Link>
 		</Box >
 	)
 }
