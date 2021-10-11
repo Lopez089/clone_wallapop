44a66ca251e543fcfcc58eb04a5962546c7fe833
Juan Lopez <aragonlopezjuan87@gmail.com>
2021-10-11 15:07:32 +0200

    style: onchage list to article

--- src/container/productContainer.jsx
+++ src/container/productContainer.jsx
@@ -8,7 +8,15 @@ export const ProductContainer = () => {
 
 	return (
 		<SimpleGrid columns={2} spacing={'.5rem'}>
-			{articles.map((article) => <Product image={article.image} name={article.name} key={article.id} price={article.price} id={article.id} like={article.like} />)}
+			{articles.map((article) =>
+				<Product
+					image={article.image}
+					name={article.name}
+					key={article.id}
+					price={article.price}
+					id={article.id}
+					like={article.like}
+				/>)}
 		</SimpleGrid>
 	)
 }
